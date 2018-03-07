fde8e99dfb76223c22a429b1747d4620e228115a
leswing <lilleswing@gmail.com>
2018-03-07 13:36:34 -0500

    Templating

--- deepchemwebsite/hack_templating.py
+++ deepchemwebsite/hack_templating.py
@@ -2,7 +2,10 @@ import os
 
 
 def template_file(header_lines, html_lines):
-  html_lines = html_lines[85:]
+  index = 0
+  while not html_lines[index].startswith('<div class="container">'):
+    index += 1
+  html_lines = html_lines[index:]
   return header_lines + html_lines
 
 
@@ -22,9 +25,9 @@ def template_folder(folder, header_lines):
 
 
 def template_131():
-  header_lines = open("deepchemwebsite/1.3.1_header.html.j2").readlines()
-  #template_folder('website/docs/2.0.0', header_lines)
-  template_folder('website/docs/1.3.1', header_lines)
+  header_lines = open("deepchemwebsite/notebooks_header.html.j2").readlines()
+  # template_folder('website/docs/2.0.0', header_lines)
+  template_folder('website/docs/notebooks', header_lines)
 
 
 if __name__ == "__main__":
