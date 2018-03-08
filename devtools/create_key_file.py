54fdf3ccb2e33c16ad8f7d1f6b9f988da7fdadea
leswing <lilleswing@gmail.com>
2018-03-08 14:26:53 -0500

    Wrong key name

--- devtools/create_key_file.py
+++ devtools/create_key_file.py
@@ -4,7 +4,7 @@ import os
 with open('keys.crt', 'w') as f:
   f.write('''[default]
 access_key = {AWS_ACCESS_KEY_ID}
-secret_key = {AWS_SECRET_ACCESS_KEY}
+secret_key = {AWS_SECRET_ACCESS_KEY_ID}
 guess_mime_type = True
 no_mime_magic = True
 '''.format(**os.environ))
