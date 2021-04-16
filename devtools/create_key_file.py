1d62a3f0d2050446482183975f00045bf05c7104
alat-rights <54920181+alat-rights@users.noreply.github.com>
2021-04-17 07:27:50 +0900

    This should fix it

--- devtools/create_key_file.py
+++ devtools/create_key_file.py
@@ -1,12 +1,12 @@
 import os
 
 print(len(os.environ["AWS_ACCESS_KEY_ID"]))
-print(len(os.environ["AWS_SECRET_ACCESS_KEY_ID"]))
+print(len(os.environ["AWS_SECRET_ACCESS_KEY"]))
 
 with open('keys.crt', 'w') as f:
   f.write('''[default]
 access_key = {AWS_ACCESS_KEY_ID}
-secret_key = {AWS_SECRET_ACCESS_KEY_ID}
+secret_key = {AWS_SECRET_ACCESS_KEY}
 guess_mime_type = True
 no_mime_magic = True
 '''.format(**os.environ))
